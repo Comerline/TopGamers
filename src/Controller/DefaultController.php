eb21068d86495928aa4114f292fe7d9b90c0bbba
Imanol Romera <superwave1999@gmail.com>
2019-03-20 14:47:13 +0100

    TGS-19 Controller and action created. TGS-20 In Progress

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,18 +1,37 @@
 <?php
+
 // src/Controller/LuckyController.php
+
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends AbstractController
-{
-    public function number()
-    {
+class DefaultController extends AbstractController {
+
+    public function number() {
+
         $number = random_int(0, 100);
 
         return $this->render('lucky/number.html.twig', [
-            'number' => $number,
+                    'number' => $number,
         ]);
     }
+
+    /**
+     * Matches the site base dir
+     * @Route("/", name="tg_dashboard")
+     */
+    public function index() {
+
+        //Get the game data from the api
+        $linkenv = getenv('TG_JSON_ALLGAMES');
+        $json = file_get_contents($linkenv);
+        $gameobj = json_decode($json, true);
+        $properties = ['urltitle' => 'TopGamers Dashboard',
+            'tggames' => $gameobj];
+        return $this->render('dashboard.html.twig', $properties);
+    }
+
 }
